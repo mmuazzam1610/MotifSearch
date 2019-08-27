5958c25e363fecdcb16e66a1bb6f275c72757258
hafiz <hafiz@Terrys-PC>
2019-08-28 00:36:45 +0500

    Update MotifSearch.java

--- src/main/java/com/mycompany/motifsearch/MotifSearch.java
+++ src/main/java/com/mycompany/motifsearch/MotifSearch.java
@@ -7,24 +7,57 @@ package com.mycompany.motifsearch;
 
 import java.util.ArrayList;
 
-public class MotifSearch {
+public class MotifSearch 
+{
     static ArrayList<String> motifs;
-    static void generatePermutations(String str, String stringToPrint){
-        if(stringToPrint.length()==str.length()){
+    static void generatePermutations(String str, String stringToPrint)
+    {
+        if(stringToPrint.length()==str.length())
+        {
             motifs.add(stringToPrint);
             return;
         }
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) 
+        {
             generatePermutations(str, stringToPrint + str.charAt(i));
         }
     }
-    boolean compareMotifs(String[] inputs, String motif){
-       
-        return false;
+    static boolean compareMotifs(String[] inputs, String motif)
+    {
+        boolean[] found = {false, false, false};
+        for (int i = 0; i < inputs.length; i++)
+        {
+            String s = inputs[i];
+            for (int j = 0; j < s.length() - 3; j++)
+            {
+                int count = 0;
+                String sub = s.substring(j, j + 4);
+                for (int k = 0; k < sub.length(); k++)
+                {
+                    if (sub.substring(k, k+1).equals(motif.substring(k, k+1))) count+= 1;
+                }
+                if (count >= 3)
+                {
+                    //System.out.println(s.substring(j, j + 4));
+                    found[i] = true;
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (!found[i]) return false;
+        }
+        return true;
     }
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         motifs = new ArrayList<>();
         generatePermutations("ACTG", "");
-        
+        String arry[] = {"ACTGACGCAG", "TCACAACGGG", "GAGTCCAGTT"};
+        System.out.println("Output Set: ");
+        for (int i = 0; i < motifs.size(); i++)
+        {
+            if (compareMotifs(arry, motifs.get(i))) System.out.println(motifs.get(i));
+        }
     }
 }
